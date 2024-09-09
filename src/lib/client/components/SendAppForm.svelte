dd54a4a14a2e7719fb2ab61b127197c69e802f9e
OffMovi <max.demin.com@gmail.com>
2024-09-09 22:07:12 +0300

    Refactor form actions

---
+++ src/lib/client/components/SendAppForm.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+    import { enhance } from "$app/forms";
+    import { page } from "$app/stores";
+
+    export let pageName: string = "";
+    export let source: string = "";
+    
+    export let utm: {[key:string]: string | null} = {};
+
+    export let form: HTMLFormElement;
+
+
+    // TODO: save data after error
+</script>
+
+<form method="POST" action="?/sendapp" use:enhance bind:this={ form }>
+
+    <input type="hidden" name="page" value={ pageName }>
+    <input type="hidden" name="source" value={ source }>
+    <input type="hidden" value={ JSON.stringify(utm) }>
+    <slot />
+
+</form>
